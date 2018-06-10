549827203d04c2f5c5c6ac4b3a664cad76b04b6f
Matthieu Girod <matthieu.girod.ch@gmail.com>
2018-06-10 16:20:03 +0200

    Auth interceptor does not set token on requests to other domains

---
+++ src/app/authentication/shared/auth.interceptor.spec.ts
@@ -0,0 +1,62 @@
+import { AuthService } from './auth.service';
+import { AppConfigService } from '../../core/shared/app-config.service';
+import { inject, TestBed } from '@angular/core/testing';
+import { AuthInterceptor } from './auth.interceptor';
+import { HttpRequest } from '@angular/common/http';
+import { never } from 'rxjs/observable/never';
+
+
+const appConfigStub = {
+  getBackendUrl() {
+    return 'https://mybackend.com';
+  }
+};
+
+const authServiceStub = {
+  getToken() {
+    return '1234isNotASecureToken';
+  }
+};
+
+const nextHandler = {
+  handle(req: HttpRequest) {
+  }
+};
+
+describe('AuthInterceptor', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthInterceptor,
+        { provide: AppConfigService, useValue: appConfigStub },
+        { provide: AuthService, useValue: authServiceStub }
+      ]
+    });
+  });
+
+  it('should set token header on API requests', inject([AuthInterceptor],
+    (interceptor: AuthInterceptor) => {
+      const req = new HttpRequest('GET', 'https://mybackend.com/album/1');
+      const spy = spyOn(nextHandler, 'handle').and.callFake((r: HttpRequest) => {
+        expect(r.headers.get('X-AUTH-TOKEN')).toEqual('1234isNotASecureToken');
+        return never();
+      });
+
+      interceptor.intercept(req, nextHandler);
+
+      expect(spy).toHaveBeenCalled();
+  }));
+
+  it('should not set token header on other requests', inject([AuthInterceptor],
+    (interceptor: AuthInterceptor) => {
+      const req = new HttpRequest('GET', 'https://somedomain.com/some/route');
+      const spy = spyOn(nextHandler, 'handle').and.callFake((r: HttpRequest) => {
+        expect(r.headers.get('X-AUTH-TOKEN')).toBeNull();
+        return never();
+      });
+
+      interceptor.intercept(req, nextHandler);
+
+      expect(spy).toHaveBeenCalled();
+    }));
+});
--- src/app/authentication/shared/auth.interceptor.ts
+++ src/app/authentication/shared/auth.interceptor.ts
@@ -5,22 +5,25 @@ import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
 
 import 'rxjs/add/operator/do';
+import { AppConfigService } from '../../core/shared/app-config.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
   private static HEADER_NAME = 'X-AUTH-TOKEN';
 
+  private appConfig: AppConfigService;
   private auth: AuthService;
 
   constructor(private injector: Injector) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.appConfig = this.injector.get(AppConfigService);
     this.auth = this.injector.get(AuthService);
     const authToken = this.auth.getToken();
 
-    if (authToken) {
+    if (authToken && this.isApiRequest(req)) {
       const authReq = req.clone({headers: req.headers.set(AuthInterceptor.HEADER_NAME, authToken)});
 
       return next.handle(authReq).do(
@@ -35,5 +38,9 @@ export class AuthInterceptor implements HttpInterceptor {
     }
 
     return next.handle(req);
+  }
+
+  private isApiRequest(req: HttpRequest<any>) {
+    return req.url.startsWith(this.appConfig.getBackendUrl() + '/');
   }
 }
